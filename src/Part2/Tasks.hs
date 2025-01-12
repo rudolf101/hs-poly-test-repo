5917c9911832a53c8dd72fff321aff1419f4f9ca
Vladislav Kasimov <vladislavkasimov@gmail.com>
2025-01-12 20:33:04 +0300

    Complete part 2

--- src/Part2/Tasks.hs
+++ src/Part2/Tasks.hs
@@ -7,23 +7,41 @@ data BinaryOp = Plus | Minus | Times deriving (Show, Eq)
 data Term = IntConstant { intValue :: Int }          -- числовая константа
           | Variable    { varName :: String }        -- переменная
           | BinaryTerm  { op :: BinaryOp, lhv :: Term, rhv :: Term } -- бинарная операция
-             deriving(Show,Eq)
+             deriving (Show, Eq)
+
+-- задаем ассоциативность и приоритеты операций
+infixl 7 |+|, |-|
+infixl 8 |*|
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) = notImplementedYet
+(|+|) = BinaryTerm Plus
+
 (|-|) :: Term -> Term -> Term
-(|-|) = notImplementedYet
+(|-|) = BinaryTerm Minus
+
 (|*|) :: Term -> Term -> Term
-(|*|) = notImplementedYet
+(|*|) = BinaryTerm Times
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = notImplementedYet
+replaceVar varName replacement (Variable v)
+  | varName == v = replacement
+  | otherwise = Variable v
+replaceVar _ _ term@(IntConstant _) = term
+replaceVar varName replacement (BinaryTerm op t1 t2) =
+  BinaryTerm op (replaceVar varName replacement t1) (replaceVar varName replacement t2)
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate = notImplementedYet
+evaluate (IntConstant x) = IntConstant x
+evaluate (Variable v) = Variable v  -- переменную нельзя вычислить
+evaluate (BinaryTerm op t1 t2) =
+  case (op, evaluate t1, evaluate t2) of
+    (Plus, IntConstant x, IntConstant y)  -> IntConstant (x + y)
+    (Minus, IntConstant x, IntConstant y) -> IntConstant (x - y)
+    (Times, IntConstant x, IntConstant y) -> IntConstant (x * y)
+    _ -> BinaryTerm op (evaluate t1) (evaluate t2)  -- возвращаем упрощенное выражение, если это не константы
