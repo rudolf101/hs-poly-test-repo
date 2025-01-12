e48860f2617cb2549c0b39c8578da2cfe1313c16
Vladislav Kasimov <vladislavkasimov@gmail.com>
2025-01-12 20:53:19 +0300

    Complete part 5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -1,32 +1,31 @@
 module Part5.Tasks where
 
-import Util(notImplementedYet)
-
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl _ acc [] = acc
+myFoldl f acc (x:xs) = myFoldl f (f acc x) xs
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr _ acc [] = acc
+myFoldr f acc (x:xs) = f x (myFoldr f acc xs)
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldl (\acc x -> acc ++ [f x]) []
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myFoldl (\acc x -> acc ++ f x) []
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myFoldl (\acc xs -> acc ++ xs) []
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = myFoldl (\acc x -> x : acc) []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldl (\acc x -> if p x then acc ++ [x] else acc) []
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
-
+myPartition p = myFoldl (\(ts, fs) x -> if p x then (ts ++ [x], fs) else (ts, fs ++ [x])) ([], [])
